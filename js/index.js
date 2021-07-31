6fdad2de4eb1587eff19620f353c37cccb407d4b
huoxue1 <3343780376@qq.com>
2021-07-31 20:35:18 +0800

    add content

--- js/index.js
+++ js/index.js
@@ -4,3 +4,17 @@ function create_club() {
 function club_show() {
     $("#iframe").attr("src","./page/club_show.html")
 }
+
+function check_login() {
+    axios.post('/get_current_uid',{
+        
+    }).then(function (response) {
+        if(response.code===-1){
+            window.location = "/login.html"
+        }else{
+            alert("申请失败")
+        }
+    }).catch(function(err){
+        window.location = "/login.html"
+    })
+}
